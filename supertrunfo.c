ba133c19950e4f15cb00b099d7827f267272e695
Ikaro Sampaio <programacaoestudos93@gmail.com>
2024-10-17 17:18:33 -0300

    Criei o cadastro de cartas

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    // Variáveis para armazenar os nomes das cidades.
-    // Inicializei todas as variáveis para que não ocorra nenhum erro.
+    // INICIALIZEI AS VARIÁVEIS PARA EVITAR ERROS COMO LEITURA DE "LIXO".
     char estado = 'A'; 
     char codigo[4]; //Ex. A01, A02...           
     char cidade [50] = "Conquista"; //Nome da cidade.          
@@ -14,36 +13,26 @@ int main() {
 
     printf("QUANTAS CARTAS DESEJA CADASTRAR? ");
     scanf("%d", &num_cartas);    
-
-    // Aqui eu usei um "Loop" de repetição "for" para cadastrar as cartas e o código não ficar muito grande.
+    // AQUI EU USEI UM LOOP DE REPETIÇAO "for" PARA CADASTRAR VÁRIAS CARTAS E O CÓDIGO NÃO FICAR GRANDE
     for (int i = 0; i < num_cartas; i++) { 
-    printf("\n---CADASTRO DA CARTA %d---\n", i + 1);      
-         
-    // Coletando os dados da cidade:  
+    printf("\n---CADASTRO DA CARTA %d---\n", i + 1);    
+    // COLETANDO OS DADOS DAS CIDADES  
     printf("Digite a letra do estado (A-H): ");
     scanf(" %c", &estado);
-
     printf("Digite o código da cidade:  ");
     scanf(" %s", &codigo);
-    // O nome da cidade não aceita espaços
+    // PARA O NOME DA CIDADE ACEITAR ESPAÇOS É PRECISO USAR A FUNÇAO "fgets", EU NÃO UTILIZEI PORQUE NÃO ESTUDEI ISSO NOS CONTEÚDOS ATÉ AGORA, MAS IREI ATUALIZAR ESSE CÓDIGO NOVAMENTE.
     printf("Digite o nome da cidade: ");
-    scanf(" %s", &cidade);
-    
-    
+    scanf(" %s", &cidade);       
     printf("Digite a população: ");
-    scanf("%d", &populacao);
-    
+    scanf("%d", &populacao);    
     printf("Digite a área em km²:  ");
     scanf(" %f", &area);
-
     printf("Digite o pib da cidade: ");
     scanf(" %f", &pib);
-
     printf("Digite o número de pontos turisticos: ");
     scanf("%d", &pontos_turisticos);
-
-    // Exibindo os dados coletados:
-
+    // EXIBINDO DADOS COLETADOS
     printf("\nINFORMAÇOES DA CIDADE CADASTRADA: \n");
     printf("Estado: %c\n", estado);
     printf("Código: %s\n", codigo);
@@ -52,6 +41,10 @@ int main() {
     printf("Área em km²: %.2f Km²\n", area);
     printf("PIB: R$ %.2f\n", pib);
     printf("Número de pontos turisticos: %d\n", pontos_turisticos);
+
+    /* ESSE É O MEU TRABALHO. É A PRIMEIRA VEZ QUE TENHO CONTATO COM CÓDIGOS E ESSE DESAFIO 
+    EU FIZ COM MUITO EMPENHO. ESPERO QUE EU NÃO TENHA ERRADO TANTO E QUE O RESLTADO SEJA SATISFATÓRIO.
+    */
     
     }    
 
